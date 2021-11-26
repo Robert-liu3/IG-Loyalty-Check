6e6ad001f2c5407108144ae7fc84c4544702bf4f
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2021-11-25 20:13:54 -0500

    Update IGLoyaltyCheck.py

--- IGLoyaltyCheck.py
+++ IGLoyaltyCheck.py
@@ -1,1 +1,11 @@
-print("hello world")
+import instaloader
+
+L = instaloader.Instaloader()
+
+USER = "your_account"
+PROFILE = USER
+
+profile = instaloader.Profile.from_username(L.context, PROFILE)
+
+print("Fetching followers of profile {}.".format(profile.username))
+followers = set(profile.get_followers())
