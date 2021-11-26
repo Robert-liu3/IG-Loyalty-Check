81bafd34a8781db8e7c905645f1cc04ff76bd161
Robert-liu3 <77682309+Robert-liu3@users.noreply.github.com>
2021-11-25 22:17:48 -0500

    Update IGLoyaltyCheck.py

--- IGLoyaltyCheck.py
+++ IGLoyaltyCheck.py
@@ -2,10 +2,28 @@ import instaloader
 
 L = instaloader.Instaloader()
 
-USER = "your_account"
-PROFILE = USER
+# Login or load session
+username = input("Enter your username: ")
+password = input("Enter your password: ")
 
-profile = instaloader.Profile.from_username(L.context, PROFILE)
+L.login(username, password)
+
+
+profile = instaloader.Profile.from_username(L.context, username)
 
 print("Fetching followers of profile {}.".format(profile.username))
 followers = set(profile.get_followers())
+
+print("Fetching followings of profile {}.".format(profile.username))
+followings = set(profile.get_followees())
+
+print("Storing non loyal people you follow")
+with open("People that don't follow back", 'w') as f:
+    for fings in followings:
+        theyLoyal = 0
+        for fers in followers:
+            if (fings.username == fers.username):
+                theyLoyal = 1
+        if (theyLoyal == 0):
+            print("damn", fings.username, "ain't loyal")
+            print(fings.username, file=f)
